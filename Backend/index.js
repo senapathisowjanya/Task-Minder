147e06340ad1fb8064527a06a033b6ac1aca1c59
senapathisowjanya <sowjanyanageswar9@gmail.com>
2023-06-18 22:09:21 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,8 @@ require("dotenv").config();
 const app = express();
 const http = require('http');
 const cors=require('cors');
-const {Server}=require("socket.io")
+const {Server}=require("socket.io");
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
@@ -24,22 +25,22 @@ const io=new Server(server,{
     },
 })
 
-io.on("connection",(socket) =>{
-    console.log(`User Connected: ${socket.id}`);
-  
-    socket.on("join_room",(data)=>{
-      socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
-  
-    socket.on("send_message",(data)=>{
-      socket.to(data.room).emit("receive_message", data);
-    });
-  
-    socket.on("disconnect",()=>{
-      console.log("User Disconnected", socket.id);
-    });
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 
 app.use("/users",userRouter)
